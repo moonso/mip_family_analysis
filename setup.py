987b6c6f5afb282cee82d21924cc0c7bfdb86004
Mans Magnusson <monsunas@gmail.com>
2014-01-21 10:54:17 +0100

    Variant sorter added

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages, Command
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 
 setup(name="Mip_Family_Analysis",
@@ -9,7 +12,6 @@ setup(name="Mip_Family_Analysis",
 	author_email="mans.magnusson@scilifelab.se",
 	description=("A new tool for doing inheritance analysis and scoring in the mip pipeline."),
 	long_description = open('README.md', 'r').read(),
-    packages={'Mip_Family_Analysis', 'Mip_Family_Analysis.Utils', 'Mip_Family_Analysis.Variants', 'Mip_Family_Analysis.Family', 'Mip_Family_Analysis.Models'},
-	package_dir={'Mip_Family_Analysis': 'Mip_Family_Analysis'},
+    packages=['Mip_Family_Analysis', 'Mip_Family_Analysis.Utils', 'Mip_Family_Analysis.Variants', 'Mip_Family_Analysis.Family', 'Mip_Family_Analysis.Models'],
     scripts=['scripts/mip_family_analysis.py'],
 )
