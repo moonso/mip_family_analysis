2fb80ef389818a2f5703376fd5250138f4752587
Mans Magnusson <monsunas@gmail.com>
2014-01-27 13:53:10 +0100

    Bumped version number and fixed outfile

--- scripts/run_mip_family_analysis.py
+++ scripts/run_mip_family_analysis.py
@@ -45,14 +45,12 @@ def main():
     parser.add_argument('family_file', type=str, nargs=1, help='A pedigree file. Default is cmms format.')
     parser.add_argument('variant_file', type=str, nargs=1, help='A variant file.Default is vcf format')
     
-    parser.add_argument('-out', '--outfile', type=str, nargs=1, help='Specify the path to output, if no file specified the output will be printed to screen.')
+    parser.add_argument('-o', '--outfile', type=str, nargs=1, help='Specify the path to output, if no file specified the output will be printed to screen.')
     
     parser.add_argument('-v', '--verbose', action="store_true", help='Increase output verbosity.')
     
     parser.add_argument('-ga', '--gene_annotation', type=str, choices=['Ensembl', 'HGNC'], nargs=1, default=['HGNC'], help='What gene annotation should be used, HGNC or Ensembl.')
     
-    parser.add_argument('-o', '--output', type=str, nargs=1, help='Specify the path to a file where results should be stored.')
-
     parser.add_argument('-pos', '--position', action="store_true", help='If output should be sorted by position. Default is sorted on rank score')
 
     parser.add_argument('-tres', '--treshold', type=int, nargs=1, help='Specify the lowest rank score to be outputted.')
@@ -154,81 +152,9 @@ def main():
     if not args.outfile:
         with open(results, 'r') as f:
             for line in f:
-                print line
+                print line.rstrip()
         os.remove(results)
     
-    
-    
-    # 
-    # # Add info about variant file:
-    # new_headers = my_variant_parser.header_lines 
-    # 
-    # # Add new headers:
-    # 
-    # new_headers.append('Inheritance_model')
-    # new_headers.append('Compounds')
-    # new_headers.append('Rank_score')
-    # 
-    # 
-    # if args.verbose:
-    #     print 'Checking genetic models...'
-    #     print ''
-    # 
-    # for data in my_variant_parser.metadata:
-    #     print data
-    # 
-    # print '#'+'\t'.join(new_headers)
-    # 
-    # if not args.position:
-    #     all_variants = {}
-    # 
-    # # Check the genetic models
-    # 
-    # jobs=[]
-    # 
-    # for chrom in my_variant_parser.chrom_shelves:
-    #     
-    #     shelve_directory = os.path.split(my_variant_parser.chrom_shelves[chrom])[0]
-    #     current_shelve = my_variant_parser.chrom_shelves[chrom]
-    #     
-    #     p = multiprocessing.Process(target=check_variants, args=(current_shelve, my_family, gene_annotation, args, preferred_models))
-    #     jobs.append(p)
-    #     p.start()
-    # 
-    # for job in jobs:
-    #     job.join()
-    # 
-    # # Print all variants:
-    # 
-    # for chrom in my_variant_parser.chrom_shelves:
-    #     
-    #     variants = []        
-    #     variant_db = shelve.open(my_variant_parser.chrom_shelves[chrom])
-    #     
-    #     for var_id in variant_db:
-    #         variants.append(variant_db[var_id])
-    #         
-    #     for variant in sorted(variants, key=lambda genetic_variant:genetic_variant.start):
-    #         pass
-    #         # print '\t'.join(variant.get_cmms_variant())
-    # 
-    # 
-    #     os.remove(my_variant_parser.chrom_shelves[chrom])
-    # os.removedirs(shelve_directory)
-    # 
-    # # Else print by rank score:
-    # # if not args.position:
-    # #     for variant in sorted(all_variants.iteritems(), key=lambda (k,v): int(operator.itemgetter(-1)(v)), reverse=True):
-    # #         if args.treshold:
-    # #             rank_score = int(variant[-1][-1])
-    # #             if rank_score >= args.treshold[0]:
-    # #                 print '\t'.join(variant[1])
-    # #         else:
-    #             # print '\t'.join(variant[1])
-    # if args.verbose:
-    #     print 'Finished analysis!'
-    #     print 'Time for analysis', (datetime.now() - start_time_variant_parsing)
-
 
 
 if __name__ == '__main__':
--- setup.py
+++ setup.py
@@ -5,14 +5,17 @@ try:
 except ImportError:
     from distutils.core import setup
 
+with open('README.md') as file:
+    long_description = file.read()
 
 setup(name="mip_family_analysis",
-	version="0.4.1",
+	version="0.5",
 	author="Mans Magnusson",
 	author_email="mans.magnusson@scilifelab.se",
 	description=("A new tool for doing inheritance analysis and scoring in the mip pipeline."),
-	long_description = open('README.md', 'r').read(),
+	long_description = long_description,
     packages={'mip_family_analysis', 'mip_family_analysis.utils', 'mip_family_analysis.variants', 'mip_family_analysis.family', 'mip_family_analysis.models'},
+    url='https://github.com/moonso/Mip_Family_Analysis',
     package_dir={'mip_family_analysis':'mip_family_analysis'},
     scripts=['scripts/run_mip_family_analysis.py'],
 )
